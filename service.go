bf200d49b4a9a1a7002d33fd07a58a7792d3598f
Craig Hesling <craig@hesling.com>
2017-05-31 13:05:44 -0400

    Added StartDeviceUpdates

--- service.go
+++ service.go
@@ -3,14 +3,22 @@ package framework
 
 import (
 	"errors"
-	"fmt"
 	"log"
 	"math/big"
 
 	CRAND "crypto/rand"
 
+	"os"
+
+	"encoding/json"
+
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 	"github.com/openchirp/framework/rest"
+)
+
+const (
+	deviceUpdatesBuffering = 10
+	mqttPersistence        = false // we should never have this enabled
 )
 
 /* Options to be filled in by arguments */
@@ -21,15 +29,31 @@ var mqttQos uint
 
 var ErrNotImplemented = errors.New("This method is not implemented yet")
 
+const (
+	// DeviceUpdateAdd indicates that a new device linked in this service
+	DeviceUpdateTypeAdd = iota
+	// DeviceUpdateRem indicates a device has unlinked this service
+	DeviceUpdateTypeRem
+	// DeviceUpdateUpd indicates that a device this service's config
+	DeviceUpdateTypeUpd
+)
+
+type DeviceUpdate struct {
+	Type int
+	ServiceDeviceUpdate
+}
+
 type Service struct {
+	id      string
 	host    rest.Host
 	mqtt    MQTT.Client
 	node    rest.ServiceNode
-	devices []rest.ServiceDeviceListItem
+	updates chan DeviceUpdate
+	log     *log.Logger
 }
 
-/* Generate a random client id for mqtt */
-func (s Service) genclientid() string {
+// genclientid generates a random client id for mqtt
+func (s Service) genClientID() string {
 	r, err := CRAND.Int(CRAND.Reader, new(big.Int).SetInt64(100000))
 	if err != nil {
 		log.Fatal("Couldn't generate a random number for MQTT client ID")
@@ -51,12 +75,15 @@ func StartService(host rest.Host, serviceid string) (*Service, error) {
 	var err error
 
 	s := new(Service)
+	s.id = serviceid
+	s.log = log.New(os.Stderr, "Service:", log.Flags())
+
 	// we should expect mqtt settings to come from framework host
 	// for now, we will simply deduce it from framework Host
 	// url.Parse(host.)
 
 	// Get Our Service Info
-	s.node, err = host.RequestServiceInfo(serviceid)
+	s.node, err = host.RequestServiceInfo(s.id)
 	if err != nil {
 		return nil, err
 	}
@@ -64,7 +91,7 @@ func StartService(host rest.Host, serviceid string) (*Service, error) {
 	// Connect to MQTT
 	/* Setup basic MQTT connection */
 	opts := MQTT.NewClientOptions().AddBroker(s.node.Properties["MQTTBroker"])
-	opts.SetClientID(s.genclientid())
+	opts.SetClientID(s.genClientID())
 	opts.SetUsername(s.node.Properties["MQTTUser"])
 	opts.SetPassword(s.node.Properties["MQTTPass"])
 
@@ -77,27 +104,118 @@ func StartService(host rest.Host, serviceid string) (*Service, error) {
 	// Subscribe to our news topic
 	// s.mqtt.Subscribe()
 
-	// Get The Current Device Config
-	s.devices, err = host.RequestServiceDeviceList(serviceid)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Println(s.devices)
-
 	return s, nil
 }
 
-func (s *Service) StartNews() {
+func (s *Service) StartDeviceUpdates() (<-chan DeviceUpdate, error) {
+	s.updates = make(chan DeviceUpdate, deviceUpdatesBuffering)
+	// Hack until we have one unified topic
+	addtopic := s.node.Pubsub.Topic + "/thing/new"
+	remtopic := s.node.Pubsub.Topic + "/thing/remove"
+	updtopic := s.node.Pubsub.Topic + "/thing/update"
+	err := s.Subscribe(addtopic, func(service *Service, topic string, payload []byte) {
+		var mqttMsg ServiceUpdatesEncapsulation
+		err := json.Unmarshal(payload, &mqttMsg)
+		if err != nil {
+			s.log.Printf("Failed to unmarshal message on topic %s\n", topic)
+			return
+		}
+		s.updates <- DeviceUpdate{
+			Type:                DeviceUpdateTypeAdd,
+			ServiceDeviceUpdate: mqttMsg.Thing,
+		}
+	})
+	if err != nil {
+		close(s.updates)
+		s.updates = nil
+	}
 
+	err = s.Subscribe(remtopic, func(service *Service, topic string, payload []byte) {
+		var mqttMsg ServiceUpdatesEncapsulation
+		err := json.Unmarshal(payload, &mqttMsg)
+		if err != nil {
+			s.log.Printf("Failed to unmarshal message on topic %s\n", topic)
+			return
+		}
+		s.updates <- DeviceUpdate{
+			Type:                DeviceUpdateTypeRem,
+			ServiceDeviceUpdate: mqttMsg.Thing,
+		}
+	})
+	if err != nil {
+		s.Unsubscribe(addtopic)
+		close(s.updates)
+		s.updates = nil
+	}
+
+	err = s.Subscribe(updtopic, func(service *Service, topic string, payload []byte) {
+		var mqttMsg ServiceUpdatesEncapsulation
+		err := json.Unmarshal(payload, &mqttMsg)
+		if err != nil {
+			s.log.Printf("Failed to unmarshal message on topic %s\n", topic)
+			return
+		}
+		s.updates <- DeviceUpdate{
+			Type:                DeviceUpdateTypeUpd,
+			ServiceDeviceUpdate: mqttMsg.Thing,
+		}
+	})
+	if err != nil {
+		s.Unsubscribe(addtopic)
+		s.Unsubscribe(remtopic)
+		close(s.updates)
+		s.updates = nil
+	}
+
+	return s.updates, err
+}
+
+func (s *Service) StopDeviceUpdates() {
+	// Hack until we have one unified topic
+	addtopic := s.node.Pubsub.Topic + "/thing/new"
+	remtopic := s.node.Pubsub.Topic + "/thing/remove"
+	updtopic := s.node.Pubsub.Topic + "/thing/update"
+	s.Unsubscribe(addtopic)
+	s.Unsubscribe(remtopic)
+	s.Unsubscribe(updtopic)
+	close(s.updates)
+}
+
+func (s *Service) FetchDeviceConfigs() ([]rest.ServiceDeviceListItem, error) {
+	// Get The Current Device Config
+	devs, err := s.host.RequestServiceDeviceList(s.id)
+	return devs, err
+}
+
+// StopService shuts down a started service
+func (s *Service) StopService() {
+	s.mqtt.Disconnect(0)
+}
+
+type TopicHandler func(service *Service, topic string, payload []byte)
+
+func (s *Service) Subscribe(topic string, callback TopicHandler) error {
+	token := s.mqtt.Subscribe(topic, byte(mqttQos), func(client MQTT.Client, message MQTT.Message) {
+		callback(s, message.Topic(), message.Payload())
+	})
+	token.Wait()
+	return token.Error()
+}
+
+func (s *Service) Unsubscribe(topic string) error {
+	token := s.mqtt.Unsubscribe(topic)
+	token.Wait()
+	return token.Error()
+}
+
+func (s *Service) Publish(topic string, payload []byte) error {
+	token := s.mqtt.Publish(topic, byte(mqttQos), mqttPersistence, payload)
+	token.Wait()
+	return token.Error()
 }
 
 func (s *Service) GetMQTTClient() *MQTT.Client {
 	return &s.mqtt
-}
-
-// Stop shuts down the
-func (s *Service) Stop() {
-	s.mqtt.Disconnect(0)
 }
 
 // need service go routine to listen for updates
