0a4f39ec4e119ec95bb108f97ec76f7d05d9ba8f
Craig Hesling <craig@hesling.com>
2017-09-06 00:13:06 -0400

    Added basic and generic user library interface

---
+++ user.go
@@ -0,0 +1,38 @@
+package framework
+
+// UserClient represents the context for a single user client session
+type UserClient struct {
+	Client
+}
+
+// UserClientTopicHandler is a function prototype for a subscribed topic callback
+type UserClientTopicHandler func(client *UserClient, topic string, payload []byte)
+
+// StartUserClient starts the user client management layer
+func StartUserClient(frameworkuri, brokeruri, id, token string) (*UserClient, error) {
+	c := new(UserClient)
+	err := c.startClient(frameworkuri, brokeruri, id, token)
+	return c, err
+}
+
+// StopClient shuts down a started user client
+func (c *UserClient) StopClient() {
+	c.stopClient()
+}
+
+// Subscribe registers a callback for a receiving a given mqtt topic payload
+func (c *UserClient) Subscribe(topic string, callback UserClientTopicHandler) error {
+	return c.subscribe(topic, func(topic string, payload []byte) {
+		callback(c, topic, payload)
+	})
+}
+
+// Unsubscribe deregisters a callback for a given mqtt topic
+func (c *UserClient) Unsubscribe(topic string) error {
+	return c.unsubscribe(topic)
+}
+
+// Publish publishes a payload to a given mqtt topic
+func (c *UserClient) Publish(topic string, payload []byte) error {
+	return c.publish(topic, payload)
+}
